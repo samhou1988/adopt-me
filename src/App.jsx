52dabd2450979e5bd20e2c08de8150448be61af3
samhou1988 <samhou1988@gmail.com>
2022-11-29 15:14:07 +0800

    react strict mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import SearchParams from './SearchParams';
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt me!</h1>
-      <SearchParams />
-    </div>
+    <React.StrictMode>
+      <div>
+        <h1>Adopt me!</h1>
+        <SearchParams />
+      </div>
+    </React.StrictMode>
   );
 };
 
