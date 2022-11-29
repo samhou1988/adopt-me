7b19d21a11dd33c18babdb44176cfb036f03f300
samhou1988 <samhou1988@gmail.com>
2022-11-29 14:23:15 +0800

    jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { createRoot } from 'react-dom/client';
+import Pet from './Pet';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+    </div>
+  )
+};
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
