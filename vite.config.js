75ba5b7f461b7e17f0a8d8d7e4bef965aaabd652
samhou1988 <samhou1988@gmail.com>
2022-11-29 18:01:30 +0800

    configure alias for vite and eslint

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import alias from '@rollup/plugin-alias';
+import { resolve } from 'path';
+
+const projectRootDir = resolve(__dirname);
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [alias(), react()],
+  resolve: {
+    alias: {
+      '@': resolve(projectRootDir, 'src'),
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 10086,
+    open: true,
+    cors: true,
+  },
 });
