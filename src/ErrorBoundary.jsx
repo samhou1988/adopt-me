47340a69004a69acce8d14d74b8fa498ac4d97af
samhou1988 <samhou1988@gmail.com>
2022-11-29 16:07:26 +0800

    portal and ref

--- src/ErrorBoundary.jsx
+++ src/ErrorBoundary.jsx
@@ -1,5 +1,5 @@
-import { Component } from "react";
-import { Link } from "react-router-dom";
+import { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class ErrorBoundary extends Component {
   state = { hasError: false };
@@ -7,14 +7,13 @@ class ErrorBoundary extends Component {
     return { hasError: true };
   }
   componentDidCatch(error, info) {
-    console.error("ErrorBoundary caught an error", error, info);
+    console.error('ErrorBoundary caught an error', error, info);
   }
   render() {
     if (this.state.hasError) {
       return (
         <h2>
-          There was an error with this listing. <Link to="/">Click here</Link>{" "}
-          to back to the home page.
+          There was an error with this listing. <Link to="/">Click here</Link> to back to the home page.
         </h2>
       );
     }
---
+++ src/Modal.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useRef } from 'react';
+import { createPortal } from 'react-dom';
+
+const Modal = ({ children }) => {
+  const elRef = useRef(null);
+  if (!elRef.current) {
+    const modal = document.createElement('div');
+    modal.id = 'modal';
+    elRef.current = modal;
+  }
+
+  useEffect(() => {
+    const body = document.body;
+    body.appendChild(elRef.current);
+    return () => body.removeChild(elRef.current);
+  }, []);
+
+  return createPortal(<div>{children}</div>, elRef.current);
+};
+
+export default Modal;
